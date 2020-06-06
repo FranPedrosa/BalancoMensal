1c8154ac1fde69c83e0f13af67807cfd8cec286f
Moreirabf <bruno@LAPTOP-ALB11O96.localdomain>
2020-06-06 11:14:08 -0300

    add SRC

---
+++ src/Fixa.java
@@ -0,0 +1,58 @@
+
+public class Fixa {
+	private String Nome;	
+	private int[] diaMes;
+	private int duracao;
+	private int valor;
+	private int categoria;
+	
+	Fixa(String n,int d, int m, int dur, int v, int cat){
+		Nome = n;
+		diaMes[0] = d;
+		diaMes[1] = m;
+		duracao = dur;
+		valor = v;
+		categoria = cat;
+	}
+	
+	void setNome(String s) {
+		Nome = s;
+	}
+	
+	void setDiaMes(int[] d) {
+		diaMes = d;
+	}
+
+	void setDuracao(int d) {
+		duracao = d;
+	}
+
+	void setValor(int v) {
+		valor = v;
+	}
+
+	void setCategoria(int c) {
+		categoria = c;
+	}
+	
+	public String getNome() {
+		return Nome;
+	}
+	
+	public int[] getDiaMes() {
+		return diaMes;
+	}
+	
+	public int getDuracao() {
+		return duracao;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+	
+	public int getCategoria() {
+		return categoria;
+	}
+	
+}
