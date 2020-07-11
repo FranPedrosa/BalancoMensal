55b2803eb90c02ac56c6f5a78bf553b318f25f15
Fran Pedrosa <franfpedrosa@gmail.com>
2020-07-11 14:18:51 -0300

    TelaPrincipal: Salva os dados sempre que voltar para Tela Principal.

--- BalancoMensal/app/src/main/java/com/example/balancomensal/TelaPrincipal.java
+++ BalancoMensal/app/src/main/java/com/example/balancomensal/TelaPrincipal.java
@@ -19,6 +19,8 @@ public class TelaPrincipal {
         this.app = app;
         this.db = d;
 
+        db.salvar(app);
+        System.out.println("Salvando...............");
         app.setContentView(R.layout.index);
 
         Grafico g = app.findViewById(R.id.grafico);
