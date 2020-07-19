41dad0df0109f96e3e462c6aaf281f9744e5b830
Fran Pedrosa <franfpedrosa@gmail.com>
2020-07-19 12:14:58 -0300

    MainActivity: Acerta o getMonthValue com -1 em algum lugar

--- BalancoMensal/app/src/main/java/com/example/balancomensal/MainActivity.java
+++ BalancoMensal/app/src/main/java/com/example/balancomensal/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         if(db == null){
             LocalDate ld = LocalDate.now();
-            db = new Dados(ld.getMonthValue(),ld.getYear());
+            db = new Dados(ld.getMonthValue()-1,ld.getYear());
             System.out.println("Dados não encontrados criando novos.");
             //Deveria avisar o cliente que não achou o histórico de transações?
         }
@@ -41,47 +41,5 @@ public class MainActivity extends AppCompatActivity {
             new AddData(app,db);
          ====================================================================*/
     }
-
-
-/*
-    public void save(View v) {
-        TextView tv2 = findViewById(R.id.textView2);
-        Dados db = new Dados();
-        db.setTexto((String) tv2.getText());
-
-    public void salvar(Dados db) {
-        try{
-            FileOutputStream arq;
-            //Creating stream and writing the object
-            arq = openFileOutput("f.txt", Context.MODE_PRIVATE);
-
-            ObjectOutputStream out = new ObjectOutputStream(arq);
-            out.writeObject(db);
-            out.flush();
-            //closing the stream
-            out.close();
-            Toast toast = Toast.makeText(getApplicationContext(), "Sucesso" , Toast.LENGTH_SHORT);
-            toast.show();
-        }catch(Exception e){Toast toast = Toast.makeText(getApplicationContext(), "Sem sucesso" , Toast.LENGTH_SHORT);
-            toast.show();}
-
-    }
-
-
-    /*public void abrir{
-
-        try{
-            //Creating stream to read the object
-            ObjectInputStream in = new ObjectInputStream(openFileInput("f.txt"));
-            Dados db=(Dados)in.readObject();
-            //printing the data of the serialized object
-            object.setText(db.texto);
-            //closing the stream
-            in.close();
-            Toast toast = Toast.makeText(getApplicationContext(), "Sucesso" , Toast.LENGTH_SHORT);
-            toast.show();
-        }catch(Exception e){System.out.println(e); Toast toast = Toast.makeText(getApplicationContext(), "Sem sucesso" , Toast.LENGTH_SHORT);
-            toast.show();}
-    }*/
-
 }
+
